b4958d6d7a6c177a301ab11479f8fa958f515f74
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-01-19 17:00:42 +0600

    react

---
+++ m44/tailwind-react-pricing-options/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import DaisyNav from "./components/DaisyNav/DaisyNav";
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      <header> 
+         <NavBar></NavBar>
+         <DaisyNav></DaisyNav>
+        <h1 className="text-7xl underline">Vite + React</h1>
+      </header>
+      <main>
+      
+
+      </main>
+    </>
+  );
+}
+
+export default App;
